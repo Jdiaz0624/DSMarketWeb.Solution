5a5f2c21a3d13f66c6d6126e33a2293f2befe302
Ing. Juan Marcelino <Ing. Juan Marcelino@JDIAZ0624>
2020-11-23 23:01:39 -0400

    Nacimiento

---
+++ DSMarketWeb.Data/ConexionADO/BDConexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DSMarketWeb.Data.ConexionADO
+{
+    public static class BDConexion
+    {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection Conexion = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DSMarketConexionWEB"].ConnectionString);
+            Conexion.Open();
+            return Conexion;
+        }
+    }
+}
---
+++ DSMarketWeb.Logic/Comunes/Mail.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace DSMarketWeb.Logic.Comunes
+{
+    public class Mail
+    {
+        MailMessage Mensaje = new MailMessage();
+        SmtpClient smtp = new SmtpClient();
+
+        private string CorreoOrigen = "";
+        private string ClaveCorreoOrigen = "";
+        private string CorreoDestino = "";
+        private string CuerpoCorreo = "";
+        private string AsuntoCorreo = "";
+        private string SMTP = "";
+        private int Puerto = 0;
+
+        public Mail(
+            string CorreoOrigenCON,
+            string ClaveCOrreoOrigenCON,
+            string CorreoDestinoCON,
+            string CorreoCuerpoCON,
+            string AsuntoCorreoCON,
+            string SMTPCON,
+            int PuertoCON)
+        {
+            CorreoOrigen = CorreoOrigenCON;
+            ClaveCorreoOrigen = ClaveCOrreoOrigenCON;
+            CorreoDestino = CorreoDestinoCON;
+            CuerpoCorreo = CorreoCuerpoCON;
+            AsuntoCorreo = AsuntoCorreoCON;
+            SMTP = SMTPCON;
+            Puerto = PuertoCON;
+        }
+
+        public bool EnviarCorreo()
+        {
+            try
+            {
+                Mensaje.From = new MailAddress(CorreoOrigen);
+                Mensaje.Bcc.Add(new MailAddress(CorreoDestino));
+                Mensaje.Subject = AsuntoCorreo;
+                Mensaje.Body = CuerpoCorreo;
+                smtp.Host = SMTP;
+                smtp.Port = Puerto;
+                smtp.Credentials = new NetworkCredential(CorreoOrigen, ClaveCorreoOrigen);
+                smtp.EnableSsl = true;
+                //    smtp.Timeout = 999999999;
+                smtp.Send(Mensaje);
+                Mensaje.Bcc.Clear();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
